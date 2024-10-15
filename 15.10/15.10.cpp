6c54e8dc01477337d3722de6e97d3ea21b9a9b2b
kutail <kutep0v.vlad.m@gmail.com>
2024-10-15 13:32:34 +0500

    chto

--- 15.10/15.10.cpp
+++ 15.10/15.10.cpp
@@ -1,13 +1,47 @@
 ﻿#include <iostream>
 #include <Windows.h>
 
+void Mass(char arr[], const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
+
+void Mass(double arr[], const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
+
+void Mass(int arr[], const int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
+
+
+
+
 int main()
 {
     SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
+    SetConsoleOutputCP(1251);   
 
+    const int size = 5;
+    int arr[size]{3, 2 ,1 ,2 ,3};
+    char arr[size]{ 'g', 'a', 'd', 'f', 'x'};
+    double arr[size]{3.12, 4.12, 5.24, 12.23, 2.12};
 
-    std::cout << "Hello World!\n";
+    Mass(arr, size);
 
     return 0;
 }
